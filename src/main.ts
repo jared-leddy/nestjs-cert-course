59a2ff356ae4867702849e3e92bd97c83c92c490
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-10 23:09:00 -0400

    setup validation pipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true, // Throw errors when whitelisted properties are found
+      transform: false, // Enabling auto transform feature of ValidationPipe
+      whitelist: true, // Enabling "whitelist" feature of ValidationPipe
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
