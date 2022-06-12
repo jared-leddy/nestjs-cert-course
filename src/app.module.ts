47a7f0eb4d96421a251edc3f48214b3bdbfcc6d3
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-12 01:27:33 -0400

    setup async db loading to not worry about order of operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,15 +14,18 @@ import { DatabaseModule } from './database/database.module';
       // load: [appConfig], // used when a config.ts file is in place
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres', // type of our database
-      host: process.env.DATABASE_HOST, // database host
-      port: process.env.DATABASE_PORT, // database host
-      username: process.env.DATABASE_USER, // username
-      password: process.env.DATABASE_PASSWORD, // user password
-      database: process.env.DATABASE_NAME, // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically
-      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    TypeOrmModule.forRootAsync({
+      // use forRootAsync to remove order of operations requirements in the imports array
+      useFactory: () => ({
+        type: 'postgres', // type of our database
+        host: process.env.DATABASE_HOST, // database host
+        port: process.env.DATABASE_PORT, // database host
+        username: process.env.DATABASE_USER, // username
+        password: process.env.DATABASE_PASSWORD, // user password
+        database: process.env.DATABASE_NAME, // name of our database,
+        autoLoadEntities: true, // models will be loaded automatically
+        synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+      }),
     }),
     CoffeeRatingModule,
     DatabaseModule,
