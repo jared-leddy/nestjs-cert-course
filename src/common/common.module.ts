02be4e1a48c3a7be9807777ac8c21b29c4bb668d
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-12 09:59:43 -0400

    setup custom middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { APIKeyGuard } from './guards/api-key.guard';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   providers: [APIKeyGuard],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // all routes
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+    // all coffees routes, only get requests
+    // consumer
+    //   .apply(LoggingMiddleware)
+    //   .forRoutes({ path: 'coffees', method: RequestMethod.GET });
+    // exclude coffees, global apply for everything else
+    // consumer.apply(LoggingMiddleware).exclude('coffees').forRoutes('*');
+  }
+}
