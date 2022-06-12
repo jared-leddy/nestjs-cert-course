6d7839c3f2959291940bbac83f1b7306c7461503
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-12 05:59:15 -0400

    create interceptors for testing

---
+++ src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,24 @@
+// import {
+//   CallHandler,
+//   ExecutionContext,
+//   Injectable,
+//   NestInterceptor,
+//   RequestTimeoutException,
+// } from '@nestjs/common';
+// import { Observable, TimeoutError } from 'rxjs';
+// import { catchError, timeout } from 'rxjs/operators';
+
+// @Injectable()
+// export class TimeoutInterceptor implements NestInterceptor {
+//   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+//     return next.handle().pipe(
+//       timeout(3000),
+//       catchError((err) => {
+//         if (err instanceof TimeoutError) {
+//           return new RequestTimeoutException();
+//         }
+//         return new Error(err);
+//       }),
+//     );
+//   }
+// }
