1c3759d1e7e4352cca9c36417cc980df8f94200b
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-12 02:05:48 -0400

    setup http exception handler

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,29 @@
+import {
+  Catch,
+  HttpException,
+  ExceptionFilter,
+  ArgumentsHost,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter<T extends HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp(); // catch in-flight
+    const response = ctx.getResponse<Response>(); // get express.js type response
+
+    const status = exception.getStatus(); // get status code
+    const exceptionResponse = exception.getResponse(); // get string message response
+    const error =
+      typeof response === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as object);
+
+    response.status(status).json({
+      ...error,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true, // Throw errors when whitelisted properties are found
